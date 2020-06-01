78453219448db4ce1637f1542721646032f6a7f9
David Enoma <davidenoma@gmail.com>
2020-06-01 05:03:06 +0100

    first stage with bugs on the vue front end

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>hey</title>
+
+        <!-- Fonts -->
+   
+ <!-- <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}"> -->
+      
+    </head>
+    <body>
+            <div id="app">
+                
+            </div>  
+        
+   
+
+        <script src="{{ mix('dist/js/main.js') }}"> 
+
+        </script>
+    </body>
+</html>
